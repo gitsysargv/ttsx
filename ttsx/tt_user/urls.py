ff22966e1b51dd9a9158af3c770c877be69c189b
柴明 <mail_cm@foxmail.com>
2017-07-31 18:32:15 +0800

    把静态文件的路径设置成了列表

--- ttsx/tt_user/urls.py
+++ ttsx/tt_user/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', views.login),
+    url(r'^register/$', views.register),
 ]
--- ttsx/tt_user/views.py
+++ ttsx/tt_user/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'tt_user/login.html')
+
+
+def register(request):
+    return render(request, 'tt_user/register.html')
