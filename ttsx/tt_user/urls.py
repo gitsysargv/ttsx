f49529472f8df1ac6264ff2ffeec88c28fa34981
柴明 <mail_cm@foxmail.com>
2017-08-03 10:06:31 +0800

    修改登录注册的模板的头部不显示父模板的header_top

--- ttsx/tt_user/urls.py
+++ ttsx/tt_user/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^register/$', views.register),
     url(r'^handle/$', views.handle),
     url(r'^reuse/$', views.reuse),
+    url(r'^$', views.user_center_info),
+    url(r'^login_handle/$', views.login_handle),
+    url(r'^user_center_order/$', views.user_center_order),
+    url(r'^user_center_site/$', views.user_center_site),
 ]
--- ttsx/tt_user/views.py
+++ ttsx/tt_user/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 
 
 def login(request):
-    return render(request, 'tt_user/login.html')
+    return render(request, 'tt_user/login.html', {'header_top': '0'})
 
 
 def handle(request):
@@ -16,7 +16,7 @@ def handle(request):
     email = d.get('email')
 
     if upwd != ucpwd:
-        return redirect('tt_user/register.html')
+        return redirect('tt_user/register.html', {'header_top': '0'})
 
     s1 = sha1()
     s1.update(upwd.encode())
@@ -27,11 +27,11 @@ def handle(request):
     info.upwd = pwd_hash
     info.email = email
     info.save()
-    return render(request, 'tt_user/login.html')
+    return render(request, 'tt_user/login.html', {'header_top': '0'})
 
 
 def register(request):
-    return render(request, 'tt_user/register.html')
+    return render(request, 'tt_user/register.html', {'header_top': '0'})
 
 
 def reuse(request):
@@ -39,6 +39,38 @@ def reuse(request):
     print(get_name)
     info = UserInfo.objects.filter(uname=get_name)
     if info:
+        # 当数据库中有重名的用户名时返回一个错误
         return JsonResponse({'reuse': 1})
     else:
         return JsonResponse({'reuse': 0})
+
+
+def user_center_info(request):
+
+    return render(request, 'tt_user/user_center_info.html')
+
+
+def login_handle(request):
+    user_path = request.session.get('user-path', '/')  # 取出session里边存储的前一次路径
+    uname = request.POST.get('username')
+    upwd = request.POST.get('pwd')
+
+    info = UserInfo.objects.filter(uname=uname)
+    if len(info) == 0:
+        # 当有错误的时候传递数据给模板，传之前错误的名字
+        return render(request, 'tt_user/login.html', {'user_error': 1, 'username': uname, 'header_top': '0'})
+    else:
+        s1 = sha1()
+        s1.update(upwd.encode())
+        upwd_sha1 = s1.hexdigest()
+        if info[0].upwd != upwd_sha1:
+            return render(request, 'tt_user/login.html', {'pwd_error': 1, 'upwd': upwd, 'header_top': '0'})
+    return redirect(user_path)
+
+
+def user_center_order(request):
+    pass
+
+
+def user_center_site(request):
+    pass
